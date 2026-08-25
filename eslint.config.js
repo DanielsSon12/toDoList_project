a1db17e06ef0c289c50d0459620db51fdbb8b2b8
Daniel_Ouver <DANIELOUVERNEY67@GMAIL.COM>
2026-08-24 22:15:04 -0300

    chore: configura eslint e dependências

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,5 +21,26 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
+  prettierConfig,
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import './App.css';
-
 function App() {
   return (
-    <h1>ToDo List</h1>
+    <main className="bg-gray-900">
+      <div className="text-center">
+        <h1 className="cursor-default pt-10 text-5xl">TODO LIST</h1>
+        <p className="py-8 text-center text-3xl">
+          Organize seus deveres e compromissos com a ToDo List
+        </p>
+      </div>
+      <div>{/* todo */}</div>
+    </main>
   )
 }
 
